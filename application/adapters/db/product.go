66cd676705e4d84d6fae708dfdeddd3a702ab70a
Victor Lellis <vmlellis@gmail.com>
2022-01-08 20:56:29 -0300

    test: db product get

--- application/adapters/db/product.go
+++ application/adapters/db/product.go
@@ -13,6 +13,10 @@ type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
 func (p *ProductDb) Get(id string) (contract.ProductInterface, error) {
 	var product entity.Product
 	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
---
+++ application/adapters/db/product_test.go
@@ -0,0 +1,54 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/vmlellis/go-hexagonal/application/adapters/db"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products (
+		"id" string,
+		"name" string,
+		"price" float,
+		"status" string
+	);`
+
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `INSERT INTO products VALUES("abc", "Product Test", 0, "disabled")`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func TestProductDB_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, float64(0), product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+}
