566a9ce6bb12af1ae7f0cdff1afbdb2520d4f046
Victor Lellis <vmlellis@gmail.com>
2022-01-09 15:23:15 -0300

    feat: cli

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM golang:1.16
 WORKDIR /go/src
 ENV PATH="/go/bin:${PATH}"
 
-RUN go install github.com/spf13/cobra/cobra@latest && \
+RUN go install github.com/spf13/cobra/cobra@v1.3.0 && \
     go install github.com/golang/mock/mockgen@v1.5.0
 
 RUN apt-get update && apt-get install sqlite3 -y
--- main.go
+++ main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2022 Victor Lellis <vmlellis@gmail.com>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/vmlellis/go-hexagonal/application"
-	"github.com/vmlellis/go-hexagonal/application/adapters/db"
-)
+import "github.com/vmlellis/go-hexagonal/cmd"
 
 func main() {
-	dbInstance, _ := sql.Open("sqlite3", "sqlite.db")
-	productDbAdapter := db.NewProductDb(dbInstance)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Exemplo", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
